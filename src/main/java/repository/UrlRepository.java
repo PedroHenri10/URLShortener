43e8cc59a0d2ab7b66810d9b0e991041ebc84a90
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-16 23:14:52 -0300

    repositorio feito

--- src/main/java/repository/UrlRepository.java
+++ src/main/java/repository/UrlRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
-public interface UrlRepository {
+import model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url,Long> {
+
+    Optional<Url> findByShortUrl(Long shortUrl);
 }
