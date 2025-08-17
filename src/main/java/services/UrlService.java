3a84451bf68086efe7a773753520378842c8d2fa
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-16 23:23:44 -0300

    AMANHA TERMINO

--- src/main/java/services/UrlService.java
+++ src/main/java/services/UrlService.java
@@ -1,7 +1,20 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.UrlRepository;
 
 @Service
 public class UrlService {
+
+    @Autowired
+    private UrlRepository urlRepository;
+
+    public String shorterUrl(String originalUrl){
+        return "";
+    }
+
+    private String generateShortUrl(){
+        String characteres = "JubISCREITONREMOArseNalmenkkiaKaKAROTTOMUNASDIO";
+    }
 }
