d4e5839393fdc3156f8f8f8afd90035911d4d6f0
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-16 23:05:46 -0300

    configs inicial

---
+++ src/test/java/com/shortener/URL/UrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortener.URL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
