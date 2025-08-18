ce920d63c5da2f23d4f334ee87c6ba3f14aaee05
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-17 21:28:15 -0300

    mongo usa id

--- src/main/java/repository/UrlRepository.java
+++ src/main/java/repository/UrlRepository.java
@@ -2,10 +2,11 @@ package repository;
 
 import model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface UrlRepository extends JpaRepository<Url,Long> {
+public interface UrlRepository extends MongoRepository<Url,String> {
 
     Optional<Url> findByShortUrl(String shortUrl);
 }
