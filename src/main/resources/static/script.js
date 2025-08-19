0a61f85ffe57373c49da1751c6dfa7c336c3ae96
PedroHenri10 <dinhonoliver@gmail.com>
2025-08-19 20:56:02 -0300

    add js

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const shortenBtn = document.getElementById('shortenBtn');
+    const urlInput = document.getElementById('urlInput');
+    const resultDiv = document.getElementById('result');
+    const errorDiv = document.getElementById('error');
+    const shortUrlLink = document.getElementById('shortUrlLink');
+    const copyBtn = document.getElementById('copyBtn');
+    const feedbackDiv = document.getElementById('feedback');
+
+    
+});
